83e1095e10c3f2bbcb374eb50123936b8ac25fd8
aliyanavas <anavas@mail.lipscomb.edu>
2016-10-24 17:02:06 -0500

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
-public class Person{
 
+public class Person{
+  
   //intializing attributes 
   String lastname;
   String firstname;
   int zipcode;
 
-  public Person{
-    
+  public Person()
+  {
+    this.lastname = X;
+    this.firstname = X;
+    this.zipcode = X;
   }
   public static void main(String[] args){
 

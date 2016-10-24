2af947f769ee8026a58e5db0bd9f2923c9593159
aliyanavas <anavas@mail.lipscomb.edu>
2016-10-24 17:06:44 -0500

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,18 +1,26 @@
 import java.util.Scanner;
-
 public class Person{
-  
-  //intializing attributes 
+
+  //intializing attributes
   String lastname;
   String firstname;
   int zipcode;
 
+  //default constructor
   public Person()
   {
     this.lastname = X;
     this.firstname = X;
     this.zipcode = X;
   }
+
+  //non-default constructor 
+  public Person(String lastname, String firstname, int zipcode)
+  {
+    
+  }
+
+  //main function
   public static void main(String[] args){
 
   }
